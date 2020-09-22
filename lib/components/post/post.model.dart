45adf80cf5acdcf647d0194887d494ef4c519769
thtjwls <thtjwls@gmail.com>
2020-09-22 23:20:09 +0900

    Linter: unnecessary_getters_setters
    https://dart-lang.github.io/linter/lints/unnecessary_getters_setters.html

--- lib/components/post/post.model.dart
+++ lib/components/post/post.model.dart
@@ -2,34 +2,10 @@ import 'dart:developer';
 
 
 class Post {
-  int _userId;
-  int _id;
-  String _title;
-  String _body;
+  int userId;
+  int id;
+  String title;
+  String body;
 
   Post();
-
-  String get body => _body;
-
-  set body(String value) {
-    _body = value;
-  }
-
-  String get title => _title;
-
-  set title(String value) {
-    _title = value;
-  }
-
-  int get id => _id;
-
-  set id(int value) {
-    _id = value;
-  }
-
-  int get userId => _userId;
-
-  set userId(int value) {
-    _userId = value;
-  }
 }
