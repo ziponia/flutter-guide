f9532cf4af97dc4379c340e5dff1386de4baba9b
thtjwls <thtjwls@gmail.com>
2020-09-22 23:50:05 +0900

    #1 in progress

--- lib/components/post/posts.dart
+++ lib/components/post/posts.dart
@@ -40,14 +40,43 @@ class PostsPageState extends State<Posts> {
         appBar: AppBar(title: Text("포스트")),
         body: Center(
             child: new ListView.separated(
-                padding: EdgeInsets.all(8),
                 itemBuilder: (context, index) {
-                  return Text("$index 번째 텍스트");
+                  return PostListItem(
+                    title: "$index 번째 텍스트",
+                  );
                 },
                 separatorBuilder: (context, index) {
                   return new Divider();
                 },
                 itemCount: posts.length)),
+      ),
+    );
+  }
+}
+
+class PostListItem extends StatelessWidget {
+  final String title;
+
+  PostListItem({this.title}) : assert(title != null, "title 은 필수 입니다.");
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Container(
+                  child: Text(this.title),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
